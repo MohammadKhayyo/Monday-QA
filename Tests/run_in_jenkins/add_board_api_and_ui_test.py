8b29c5ecb6360d9aae4ca20b488703435e61fa99
MohammadKhayyo <mohammadkhayyo@gmail.com>
2024-03-28 05:38:32 +0200

    "update"

--- Tests/run_in_jenkins/add_board_api_and_ui_test.py
+++ Tests/run_in_jenkins/add_board_api_and_ui_test.py
@@ -40,11 +40,9 @@ class AddBoardTests(unittest.TestCase):
         self.work_space_name = "MY_TEAM"
         self.board_name = generate_string.create_secure_string()
         self.folder_name = "My Team"
-        self.group_name = generate_string.create_secure_string()
         self.work_space = WorkSpace(work_space_name=self.work_space_name)
         self.board = Board(work_space=self.work_space, board_name=self.board_name, folder_name=self.folder_name,
                            exists=False)
-        self.group = Group(board=self.board, group_name=self.group_name, exist=False)
         self.jira_client = JiraWrapper()
         self.test_failed = False
         self.error_msg = ""
@@ -53,53 +51,6 @@ class AddBoardTests(unittest.TestCase):
     def test_add_board(self):
         status = self.home_page.check_add_board(_name=self.board_name)
         self.assertTrue(status)
-
-    @test_decorator
-    def test_add_file(self):
-        file_path = "file1.txt"
-        data_column = {"title": "Attached Files", "column_type": "file", "description": "",
-                       "files_paths": [file_path]}
-        Column(board=self.board, title=data_column['title'], description=data_column['description'],
-               column_type=data_column['column_type'])
-        item_name = generate_string.create_secure_string()
-        item = Item(group=self.group, item_name=item_name, exist=False)
-        item.upload_files(column_title=data_column['title'], files_paths=data_column["files_paths"])
-        self.home_page.switch_board(_name=self.board_name)
-        text = self.home_page.click_Attached_Files(name_item=item.item_name)
-        with open(file_path, 'r') as file:
-            contents = file.read()
-        self.assertEqual(text, contents)
-
-    @test_decorator
-    def test_check_group(self):
-        dic_groups_via_api = self.board.get_all_group()
-        self.home_page.switch_board(_name=self.board_name)
-        list_groups_via_UI = self.home_page.get_all_group()
-        list_groups_via_api = list()
-        for group in dic_groups_via_api:
-            list_groups_via_api.append(group['title'])
-        list_groups_via_api.sort()
-        list_groups_via_UI.sort()
-        self.assertListEqual(list_groups_via_api, list_groups_via_UI)
-
-    @test_decorator
-    def test_add_link(self):
-        data_column = {"title": "Link", "column_type": "link", "description": "A link to a website",
-                       "link": "www.google.com", "placeholder": "search with google"}
-        Column(board=self.board, title=data_column['title'], description=data_column['description'],
-               column_type=data_column['column_type'])
-        item_name = generate_string.create_secure_string()
-        item = Item(group=self.group, item_name=item_name, exist=False)
-        item.add_link(column_title=data_column['title'], link=data_column['link'],
-                      description=data_column['placeholder'])
-        self.home_page.switch_board(_name=self.board_name)
-        list_links_via_UI = self.home_page.get_all_links()
-        found = False
-        for link in list_links_via_UI:
-            if link['text'] == data_column['placeholder'] and data_column['link'] in link['href']:
-                found = True
-                break
-        self.assertTrue(found)
 
     def tearDown(self):
         self.board.delete_board()
