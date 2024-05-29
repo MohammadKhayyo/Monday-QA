8bb70b76c3346eca292d0b0fa2876d0ef53d567a
MohammadKhayyo <mohammadkhayyo@gmail.com>
2024-05-29 03:30:06 +0300

    update config ui

--- Tests/run_in_jenkins/serial_epics_ui_test.py
+++ Tests/run_in_jenkins/serial_epics_ui_test.py
@@ -36,7 +36,7 @@ class SerialEpicsTests(unittest.TestCase):
         self.test_failed = False
         self.error_msg = "Reverting bulk deletion of epics failed"
 
-    # @test_decorator
+    @test_decorator
     def test_revert_bulk_epic_deletion(self):
 
         operationStatus = self.epics_Page.revertBulkDeletion()
